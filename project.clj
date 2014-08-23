2989b7b68a3d074d3f5e9bf4211defc8efa1c4e1
Ryan Sundberg <ryan.sundberg@gmail.com>
2014-08-23 14:11:57 -0700

    Add url to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject sundbry/resource "0.1.9-SNAPSHOT"
   :description "System resource library with automatic dependency resolution"
   :author "Ryan Sundberg <ryan.sundberg@gmail.com>"
+  :url "https://github.com/ryansundberg/resource"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :min-lein-version "2.1.3"
