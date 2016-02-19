7d6d1e3aee128ccb2086816584b5f088cabf2e6c
Ryan Sundberg <ryan.sundberg@gmail.com>
2016-02-18 22:40:24 -0800

    Update project to 0.4.0

--- project.clj
+++ project.clj
@@ -1,11 +1,9 @@
-(defproject sundbry/resource "0.3.1-SNAPSHOT"
+(defproject sundbry/resource "0.4.0"
   :description "System resource library with automatic dependency resolution"
   :author "Ryan Sundberg <ryan.sundberg@gmail.com>"
-  :url "https://github.com/ryansundberg/resource"
+  :url "https://github.com/sundbry/resource"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :min-lein-version "2.1.3"
   :plugins []
   :dependencies [[com.stuartsierra/dependency "0.1.1"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
-                                  [org.clojure/tools.namespace "0.2.4"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}})
